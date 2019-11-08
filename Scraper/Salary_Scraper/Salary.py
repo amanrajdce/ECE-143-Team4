b541fe89a63b81a6247eec12a99335d7811e4e11
Haoming Zhang <haomingvince@gmail.com>
2019-11-07 20:56:29 -0800

    Added code and data for salary scraper

---
+++ Scraper/Salary_Scraper/Salary.py
@@ -0,0 +1,11 @@
+class Salary:
+	def __init__(self, jobTitle, company, meanPay):
+		self.jobTitle = jobTitle
+		self.company = company
+		self.meanPay = meanPay
+		self.dic = {
+                    "jobTitle": jobTitle,
+                    "company": company,
+                    "meanPay": meanPay
+                }
+	#enddef
